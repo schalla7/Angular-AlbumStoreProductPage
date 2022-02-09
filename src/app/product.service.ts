fd3bcf4d333e085aef0eac2e300f06b48b10bff2
Schalk Sevenster <schalk.sevenster@voss-solutions.com>
2022-02-09 11:33:38 +0200

    WIP

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 
 import 'rxjs/add/operator/map';
+
+import { Album } from './album';
 
 
 @Injectable()
@@ -11,8 +14,8 @@ export class ProductService {
 
   private _albumUrl: string = '../assets/album.json';
 
-  getAlbum(id: number) {
+  getAlbum(id: number) : Observable<Album> {
     return this._http.get(this._albumUrl)
-      .map(response => response.json())
+      .map(response => <Album>response.json())
   }
 }
