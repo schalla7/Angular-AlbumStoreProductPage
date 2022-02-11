ff582cd4e49de1ba1620cfae8027c4441f202592
Schalk Sevenster <schalk.sevenster@voss-solutions.com>
2022-02-11 10:23:04 +0200

    WIP

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 
-import { Album } from '../album'
+import { Album } from '../interfaces/album'
 @Component({
   selector: 'app-product-description',
   templateUrl: './product-description.component.html',
